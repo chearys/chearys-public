97ba7638304716c49f5ab89b27fce73755f26e04
chearys <nonglekmos@gmail.com>
2024-11-17 15:31:31 +0700

    Fixed pull tracker

--- features/kuudra/rend.js
+++ features/kuudra/rend.js
@@ -40,7 +40,7 @@ registerWhen(register("SoundPlay", (a, b, c, d, e, f) => {
         if (i.includes("volcano") || i.includes("fel")) {
             sendmsg(`Back bone hit at ${formatTimeMs(getKillTime())} while holding ${Player.getHeldItem().getName()}`);
             World.playSound(Settings.backBoneSound || "mob.cat.meow", 2, 0.5);
-            Client.showTitle(Settings.pullText || "&4Pull!", "", 0, 25, 5);
+            cooldown=20;
             return;
         }
 
@@ -51,6 +51,21 @@ registerWhen(register("SoundPlay", (a, b, c, d, e, f) => {
 }).setCriteria("tile.piston.out"), () => Settings.backBone && getPhase() === 7)
 
 
+
+let cooldown = 0
+registerWhen(register("Tick", () => {
+    cooldown = Math.max(0, cooldown - 1);
+  }), () => Settings.backBone)
+
+function render() {
+
+    let a = new Text('').setScale(3).setShadow(true).setAlign('CENTER');
+    a.setString(Settings.pullText);
+    a.draw((Renderer.screen.getWidth()) / 2, (Renderer.screen.getHeight()) / 2 - 20);
+
+}
+
+registerWhen(register("renderOverlay", render), () => Settings.backBone && getPhase() === 7 && cooldown > 0)
 
 
 /**
